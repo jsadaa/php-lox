31554b1e454736323bb686c1a5911857ff602e66
Léo Paillard <103212024+jsadaa@users.noreply.github.com>
2024-09-27 22:57:51 +0200

    rename expr stmt

--- src/Parser/Stmt/ExpressionStmt.php
+++ src/Parser/Stmt/ExpressionStmt.php
@@ -7,7 +7,7 @@ namespace PHPLox\Parser\Stmt;
 use PHPLox\Parser\Expr\Expr;
 use PHPLox\Visitor\StmtVisitor;
 
-final readonly class Expression extends Stmt
+final readonly class ExpressionStmt extends Stmt
 {
     public function __construct(
         public Expr $expression, 
@@ -17,6 +17,6 @@ final readonly class Expression extends Stmt
 
     public function accept(StmtVisitor $visitor): mixed
     {
-        return $visitor->visitExpression($this);
+        return $visitor->visitExpressionStmt($this);
     }
 }
